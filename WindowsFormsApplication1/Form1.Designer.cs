bf1bd8b6c5d0c295d9a1554058095b16e8ed0d90
vencidim <vencidim@gmail.com>
2014-10-28 11:30:17 +0200

    Revert "test 1"
    This reverts commit 930ba9cd3835d484d9d54cb2b0323c80a4e39d1e.

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(81, 97);
+            this.button1.Location = new System.Drawing.Point(83, 44);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Show Message";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
